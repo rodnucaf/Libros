2ce54f8bcc5e726354766b4136ab601e65b87dbd
unknown <facundoxrabago@gmail.com>
2024-12-09 16:27:56 -0300

    Creada interfaz

---
+++ Libros/Model/ILibroRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Libros.Model
+{
+    public interface ILibroRepository
+    {
+        void Add(Libro libro);
+        void Edit(Libro libro);
+        void Delete(int id);
+
+        IEnumerable<Libro> ObtenerTodo();
+        IEnumerable<Libro> ObtenerPorValor();
+
+
+    }
+}
