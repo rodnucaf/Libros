e42b23a1e1f16a4c0c046b1e93de2b7f991715dd
unknown <facundoxrabago@gmail.com>
2024-12-09 16:29:52 -0300

    Creada interfaz

---
+++ Libros/View/ILibroView.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Libros.View
+{
+    internal interface ILibroview
+    {
+    }
+}
