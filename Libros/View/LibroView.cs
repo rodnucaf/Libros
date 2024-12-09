870d683888cbd9163a1ae935f15732134a678b8f
unknown <facundoxrabago@gmail.com>
2024-12-09 17:01:18 -0300

    Implementada la interfaz ILibroView

--- Libros/View/LibroView.cs
+++ Libros/View/LibroView.cs
@@ -10,11 +10,70 @@ using System.Windows.Forms;
 
 namespace Libros.View
 {
-    public partial class LibroView : Form
+    public partial class LibroView : Form, ILibroView
     {
         public LibroView()
         {
             InitializeComponent();
+        }
+
+        public string LibroId
+        {
+            get => throw new NotImplementedException();
+            set => throw new NotImplementedException();
+        }
+
+        public string Titulo
+        {
+            get => throw new NotImplementedException();
+            set => throw new NotImplementedException();
+        }
+        public string Autor 
+        {
+            get => throw new NotImplementedException();
+            set => throw new NotImplementedException();
+        }
+
+        public string Genero
+        {
+            get => throw new NotImplementedException();
+            set => throw new NotImplementedException();
+        }
+
+        public string BuscarPorValor
+        {
+            get => throw new NotImplementedException();
+            set => throw new NotImplementedException();
+        }
+
+        public bool IsEditar 
+        {
+            get => throw new NotImplementedException();
+            set => throw new NotImplementedException();
+        }
+
+        public bool IsCorrecto
+        {
+            get => throw new NotImplementedException();
+            set => throw new NotImplementedException();
+        }
+
+        public string Mensaje
+        {
+            get => throw new NotImplementedException();
+            set => throw new NotImplementedException();
+        }
+
+        public event EventHandler BuscarEvento;
+        public event EventHandler Agregarevento;
+        public event EventHandler EditarEvento;
+        public event EventHandler BorrarEvento;
+        public event EventHandler GuardarEvento;
+        public event EventHandler CancelarEvento;
+
+        public void SetListaLibrosBindingSource(BindingSource listaLibros)
+        {
+            throw new NotImplementedException();
         }
     }
 }
