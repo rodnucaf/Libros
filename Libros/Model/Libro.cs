7a208476f8bc44b615eba1b734fc02400d89e537
unknown <facundoxrabago@gmail.com>
2024-12-09 15:49:43 -0300

    primer commit

---
+++ Libros/Model/Libro.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Libros.Model
+{
+    public class Libro
+    {
+        //Atributos
+        private int id;
+        private string titulo;
+        private string autor;
+        private string genero;
+
+        //Propiedades
+        public int Id { get { return id; } set { id = value; } }
+        public string Titutlo { get { return titulo; } set { titulo = value; } }
+        public string Autor { get { return autor; }set { autor = value; } }
+        public string Genero { get { return genero; } set { genero = value; } }
+
+
+
+
+    }
+}
