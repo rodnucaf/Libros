c0de6535231b61a42bbbacff5eeaf45228098ec9
unknown <facundoxrabago@gmail.com>
2024-12-09 16:21:28 -0300

    Agregada referencia a DataAnnotations y agregadas validaciones sobre los atributos de la clase

--- Libros/Model/Libro.cs
+++ Libros/Model/Libro.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
 
 namespace Libros.Model
 {
@@ -14,10 +16,23 @@ namespace Libros.Model
         private string autor;
         private string genero;
 
-        //Propiedades
+        //Propiedades, validaciones
+        [DisplayName("ID Libro")]
         public int Id { get { return id; } set { id = value; } }
+
+        [DisplayName("Título")]
+        [Required(ErrorMessage = "Se requiere el título.")]
+        [StringLength(3, MinimumLength =50, ErrorMessage = "El título debe tener entre tres y cincuenta caracteres.")]
         public string Titutlo { get { return titulo; } set { titulo = value; } }
+
+        [DisplayName("Autor")]
+        [Required(ErrorMessage = "Se requiere el autor.")]
+        [StringLength(50, MinimumLength =3, ErrorMessage = "El autor debe tener entre tres y cincuenta caracteres.")]
         public string Autor { get { return autor; }set { autor = value; } }
+
+        [DisplayName("Género")]
+        [Required(ErrorMessage = "Se requiere el género")]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "El género debe tener entre tres y cincuenta caracteres.")]
         public string Genero { get { return genero; } set { genero = value; } }
 
 
