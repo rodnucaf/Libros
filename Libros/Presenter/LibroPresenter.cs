81d19bb0862f83cd85ccc8c7e930f2724f545210
unknown <facundoxrabago@gmail.com>
2024-12-10 19:23:54 -0300

    creados atributos, constructor y métodos. definidos métodos 'CargarListaLibros' y 'BuscarLibro'

--- Libros/Presenter/LibroPresenter.cs
+++ Libros/Presenter/LibroPresenter.cs
@@ -3,10 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Libros.Model;
+using Libros.View;
 
 namespace Libros.Presenter
 {
-    internal class LibroPresenter
-    {
+    public class LibroPresenter
+    {   
+        private ILibroRepository repository;
+        private ILibroView view;
+        private BindingSource libroBindingSource;
+        private IEnumerable<>
+
     }
 }
