876c4334311f4d5ef25f50524e19783060dac9df
unknown <facundoxrabago@gmail.com>
2024-12-10 23:33:08 -0300

    creado y definido constructor, creado y definido método ShowLibrosView

--- Libros/Presenter/MainPresenter.cs
+++ Libros/Presenter/MainPresenter.cs
@@ -3,10 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Libros.View;
+using Libros.Repositories;
+using Libros.Model;
+using System.Data.SqlClient;
+
 
 namespace Libros.Presenter
 {
     public class MainPresenter
     {
+        private IMainView mainView;
+        private readonly string sqlConnectionString;
+
+        public MainPresenter(IMainView mainView, string sqlConnectionString)
+        {
+            this.mainView = mainView;
+            this.sqlConnectionString = sqlConnectionString;
+
+            this.mainView.ShowLibroView += ShowLibrosView;
+        }
+
+        private void ShowLibrosView(object sender, EventArgs e)
+        {
+            ILibroView view = new LibroView();
+            ILibroRepository libroRepository = new LibroRepository(sqlConnectionString);
+            new LibroPresenter(view, libroRepository);
+        }
     }
 }
